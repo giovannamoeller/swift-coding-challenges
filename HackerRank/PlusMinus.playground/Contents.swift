46919596c9321a157e974d6a9ddbdd43f785ef5c
Giovanna <giovannagmoeller@gmail.com>
2022-04-19 17:55:48 -0300

    Hacker Rank - Challenge 01

--- HackerRank/PlusMinus.playground/Contents.swift
+++ HackerRank/PlusMinus.playground/Contents.swift
@@ -1,3 +1,34 @@
+/*:
+ ## Plus Minus
+ Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+ ### Solution
+ */
+import UIKit
+
+func lonelyInteger(a: [Int]) -> Int {
+ 
+  var lonelyValue: Int = 0
+  var b: [Int] = []
+  
+  for i in 0..<a.count {
+    for j in (i + 1)..<a.count {
+      if a[i] == a[j] {
+        b.append(a[i])
+      }
+    }
+  }
+  
+  for number in a {
+    if !(b.contains(number)) { lonelyValue = number }
+  }
+  
+  return lonelyValue
+}
+
+lonelyInteger(a: [1, 2, 3, 4, 3, 2, 1])
+lonelyInteger(a: [1, 2, 3, 3, 5, 2, 2, 5, 7, 3, 1])
+
+
 import UIKit
 
 func plusMinus(arr: [Int]) -> Void {
