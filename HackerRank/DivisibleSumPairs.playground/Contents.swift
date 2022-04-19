dfb7a8486ecb86f5fb3548175d1172de1eb60a56
Giovanna <giovannagmoeller@gmail.com>
2022-04-19 17:57:47 -0300

    Hacker Rank - Challenge 06

--- HackerRank/DivisibleSumPairs.playground/Contents.swift
+++ HackerRank/DivisibleSumPairs.playground/Contents.swift
@@ -1,9 +1,14 @@
+/*:
+ ## Divisible Sum Pairs
+ Given an array of integers and a positive integer k, determine the number of (i, j) pairs where i < j and ar[i] + ar[j] is divisible by k.
+ ### Solution
+ */
 import UIKit
 
 func divisibleSumPairs(n: Int, k: Int, ar: [Int]) -> Int {
   
   // n -> length of array ar
-  // ar -> ar
+  // ar -> array
   // k -> k
   
   var validPairs = 0
