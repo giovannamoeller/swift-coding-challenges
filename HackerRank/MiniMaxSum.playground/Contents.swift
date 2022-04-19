62ab61ce7f3c668aed075b99a345b42435ae6b91
Giovanna <giovannagmoeller@gmail.com>
2022-04-19 17:56:19 -0300

    Hacker Rank - Challenge 02

--- HackerRank/MiniMaxSum.playground/Contents.swift
+++ HackerRank/MiniMaxSum.playground/Contents.swift
@@ -1,3 +1,8 @@
+/*:
+ ## Mini-Max Sum
+ Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+ ### Solution
+ */
 import UIKit
 
 func miniMaxSum(arr: [Int]) -> Void {
