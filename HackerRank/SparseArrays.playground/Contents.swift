ab536f2ef0351ad52ffdf7bc437ca5fe7fa5087b
Giovanna <giovannagmoeller@gmail.com>
2022-04-19 17:58:05 -0300

    Hacker Rank - Challenge 07

--- HackerRank/SparseArrays.playground/Contents.swift
+++ HackerRank/SparseArrays.playground/Contents.swift
@@ -1,3 +1,8 @@
+/*:
+ ## Sparse Arrays
+ There is a collection of input strings and a collection of query strings. For each query string, determine how many times it occurs in the list of input strings. Return an array of the results.
+ ### Solution
+ */
 import UIKit
 
 func matchingStrings(strings: [String], queries: [String]) -> [Int] {
